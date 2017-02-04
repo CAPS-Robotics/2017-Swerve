c764d36bc0a1e319fd74be7735671f2ec3dca211
Ryan Pope <poperyan73@gmail.com>
2017-02-04 14:00:40 -0600

    Code that climbs

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -42,6 +42,7 @@
  *		6. If it fails, fix your errors
  */
 
+// Creating our variables
 Swerve::Swerve() {
 	this->joystick = new Joystick(0);
 	this->fl = new SwerveModule(FL_CAN, FL_PWM, true);
@@ -52,10 +53,11 @@ Swerve::Swerve() {
 	this->redLeds = new PWM(12);
 	this->greenLeds = new PWM(11);
 	this->blueLeds = new PWM(10);
-	//this->climber = new CANTalon(CLIMB_CAN);
-	//this->climber2 = new CANTalon(CLIMB2_CAN);
+	this->climber = new CANTalon(CLIMB_CAN);
+	this->climber2 = new CANTalon(CLIMB2_CAN);
 }
 
+// Inits gyro since we aren't using it
 void Swerve::RobotInit() {
 	//std::thread vision(Swerve::VisionThread);
 	//vision.detach();
@@ -63,6 +65,7 @@ void Swerve::RobotInit() {
 	gyro->Calibrate();
 }
 
+// This auto currently drives forward at 50% power
 void Swerve::Autonomous() {
 	fl->Run(0, 0.5);
 	fr->Run(0, 0.5);
@@ -75,6 +78,7 @@ void Swerve::Autonomous() {
 	br->Brake();
 }
 
+// Doesn't run yet. Don't worry about it
 void Swerve::VisionThread() {
 	cs::UsbCamera cam = CameraServer::GetInstance()->StartAutomaticCapture();
 	cam.SetResolution(320, 240);
@@ -99,31 +103,43 @@ void Swerve::VisionThread() {
 }
 
 void Swerve::OperatorControl() {
+	// Angle variables
 	float fla = 0, fra = 0, bla = 0, bra = 0;
 	double back, front, right, left;
 	while (IsOperatorControl() && IsEnabled()) {
+		// LEDs don't work so who cares, I'm not deleting it
 		this->SetRGB(0, 0, 192);
+
+		// Reading joystick input after applying a deadband
 		double strafe = this->Deadband(this->joystick->GetRawAxis(0), 0.15);
 		double forward = this->Deadband(-this->joystick->GetRawAxis(1), 0.15);
 		double rotation = this->Deadband(this->joystick->GetRawAxis(2), 0.70);
+		// Printing out joystick values
 		SmartDashboard::PutNumber("X-Value", strafe);
 		SmartDashboard::PutNumber("Y-Value", forward);
 		SmartDashboard::PutNumber("Twist", rotation);
+
+		// Speed multiplier for the memes
 		double speedMultiplier = (this->joystick->GetRawAxis(3) + 1) / 2;
 		//double heading = -gyro->GetAngle();
 		//double forward = ly * cos(heading * PI / 180) + lx * sin(heading * PI / 180);
 		//double strafe  = lx * cos(heading * PI / 180) - ly * sin(heading * PI / 180);
 
-		SmartDashboard::PutNumber("Angle fl", fl->GetEnc() * 360);
-		SmartDashboard::PutNumber("Angle fr", fr->GetEnc() * 360);
-		SmartDashboard::PutNumber("Angle bl", bl->GetEnc() * 360);
-		SmartDashboard::PutNumber("Angle br", br->GetEnc() * 360);
+		// Reading out encoder values
+		SmartDashboard::PutNumber("Angle fl", fl->GetEnc() * 180 / 120 * 360);
+		SmartDashboard::PutNumber("Angle fr", fr->GetEnc() * 180 / 120 * 360);
+		SmartDashboard::PutNumber("Angle bl", bl->GetEnc() * 180 / 120 * 360);
+		SmartDashboard::PutNumber("Angle br", br->GetEnc() * 180 / 120 * 360);
 
-		//SmartDashboard::PutNumber("Climber Current", this->climber->GetOutputCurrent());
-		//SmartDashboard::PutNumber("Climber 2 Current", this->climber2->GetOutputCurrent());
+		// Reading out current
+		SmartDashboard::PutNumber("Climber Current", this->climber->GetOutputCurrent());
+		SmartDashboard::PutNumber("Climber Temp", this->climber->GetTemperature());
+		SmartDashboard::PutNumber("Climber 2 Current", this->climber2->GetOutputCurrent());
 
+		// Speed variables
 		double flds = 0, frds = 0, blds = 0, brds = 0;
 
+		// If button 2 pressed, return wheels to zero
 		if (this->joystick->GetRawButton(2)) {
 			fl->Zero();
 			bl->Zero();
@@ -131,10 +147,12 @@ void Swerve::OperatorControl() {
 			br->Zero();
 		}
 
+		// If button 9 pressed, reset gyro
 		if (this->joystick->GetRawButton(9)) {
 			this->gyro->Reset();
 		}
 
+		// If button 1 pressed, apply the brake
 		if (this->joystick->GetRawButton(1)) {
 			fl->Brake();
 			fr->Brake();
@@ -142,6 +160,7 @@ void Swerve::OperatorControl() {
 			br->Brake();
 		}
 
+		// Press buttons 7 and 8 to zero the encoders
 		if (this->joystick->GetRawButton(7) && this->joystick->GetRawButton(8)) {
 			fl->ResetPosition();
 			fr->ResetPosition();
@@ -149,15 +168,22 @@ void Swerve::OperatorControl() {
 			br->ResetPosition();
 		}
 
-		if (this->joystick->GetRawButton(3)) {
-			this->climber->Set(0.5);
-			this->climber2->Set(0.5);
-		}
-		if (this->joystick->GetRawButton(4)) {
+		// If button 3 pressed and no crazy currents, climb
+		if (this->joystick->GetRawButton(3) && this->climber->GetOutputCurrent() <= 45 && this->climber2->GetOutputCurrent() <= 45) {
+			this->climber->Set(1);
+			this->climber2->Set(-1);
+		} else {
 			this->climber->Set(0);
 			this->climber2->Set(0);
 		}
 
+		// If button 4 pressed, or super current, don't climb
+		if (this->joystick->GetRawButton(4) || this->climber->GetOutputCurrent() > 45 || this->climber2->GetOutputCurrent() > 45) {
+			this->climber->Set(0);
+			this->climber2->Set(0);
+		}
+
+		// Finally, the swerve code
 		if (fabs(forward) != 0 || fabs(strafe) != 0 || fabs(rotation) != 0) {
 			if (fabs(rotation) != 0) {
 				back  = strafe  - rotation * 0.5;
@@ -211,11 +237,13 @@ void Swerve::OperatorControl() {
 				bra = fmod((-bra + 360), 360);
 			}
 
+			// Sets the angle and speed of the motors
 			this->fl->Run(fla, flds * speedMultiplier);
 			this->fr->Run(fra, frds * speedMultiplier);
 			this->bl->Run(bla, blds * speedMultiplier);
 			this->br->Run(bra, brds * speedMultiplier);
 		} else {
+			// If not pressing joysticks, don't move!
 			this->fl->Brake();
 			this->fr->Brake();
 			this->bl->Brake();
